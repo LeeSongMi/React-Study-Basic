9c0c44f3e385be896e21895be39bd00a38fee8d1
LeeSongMi <lssong9631@gmail.com>
2020-08-24 18:43:18 +0900

    emailJS
    emailJS

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,17 +1,69 @@
-import React from 'react';
+import React,{useState} from 'react';
+import emailjs from 'emailjs-com';
+
 
 function Food(){
   return "I Love Potato"
 }
+
+var templateParams = {
+  name:'이송미',
+  url: 'https://www.naver.com'
+};
+
+
+
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+const onChangeName = (e) => {
+  setName(e.target.value);
+  templateParams.name = e.target.value;
+}
+const onChangeEmail = (e) => {
+  setEmail(e.target.value);
+  templateParams.url=e.target.value;
+}
+
+// url
+// function sendEmail(e) {
+//   e.preventDefault();
+//   emailjs.init('user_MoH5VIvX0E93sWYwpUAdg');
+//   emailjs.send('gmail', 'template_ahN06sDM', templateParams)
+//     .then((result) => {
+//         console.log(result.text);
+//     }, (error) => {
+//         console.log(error.text);
+//     });
+// }
+const sendEmail = () => {
+  emailjs.init('user_MoH5VIvX0E93sWYwpUAdg');
+  emailjs.send('gmail', 'template_ahN06sDM', templateParams)
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+}
   return (
   <div>
     <h1>Hello</h1>
     <Food />
-
-  </div>
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden"name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name"  value={name} onChange={onChangeName} />
+      <label>Email</label>
+      <input type="email" name="user_email" value={email} onChange={onChangeEmail} />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send"/>
+    </form></div>
   )
 
 }
+
+
 
 export default App;
